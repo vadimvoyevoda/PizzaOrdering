78956952a4272578b9cdd22702b1ed5f21c5c52f
pasha369 <ermail@ukr.net>
2015-11-16 23:20:36 -0800

    Implement design to PizzaItemPage

---
+++ PizzaOnline/Models/PizzaItemModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace PizzaOnline.Models
+{
+    /// <summary>
+    /// Model to pizza item.
+    /// </summary>
+    public class PizzaItemModel
+    {
+        public Image Image { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Weight { get; set; }
+        public double Price { get; set; }
+        public int Counter { get; set; }
+    }
+}
---
+++ PizzaOnline/Views/PizzaItem.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using PizzaOnline.Models;
+
+namespace PizzaOnline.Views
+{
+    /// <summary>
+    /// Interaction logic for PizzaItem.xaml
+    /// </summary>
+    public partial class PizzaItem : UserControl
+    {
+        public PizzaItemModel Source
+        {
+            get { return (PizzaItemModel)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+        public static readonly DependencyProperty SourceProperty =
+          DependencyProperty.Register("Source", typeof(PizzaItemModel), typeof(PizzaItem), null);
+
+        public PizzaItem()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void btnUp_Click(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void btnDown_Click(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+    }
+}
