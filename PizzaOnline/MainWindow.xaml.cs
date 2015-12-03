2ecb11c2347fe74f98608b661b87845b7240710a
ilchukpasha <returnto001@gmail.com>
2015-12-03 11:11:51 +0200

    test commit

--- PizzaOnline/MainWindow.xaml.cs
+++ PizzaOnline/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PizzaOnline
-{
+{//парар
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
